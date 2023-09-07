ff87b6c815748ad75336d07cff0c7aec05bfb4dc
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-09-07 16:15:32 +0100

    Add testing file

--- src/redux/rocketSlice.js
+++ src/redux/rocketSlice.js
@@ -1,13 +1,11 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
 
 const url = 'https://api.spacexdata.com/v4/rockets';
 
 export const fetchAPI = createAsyncThunk('rockets/fetchRocket', async () => {
   try {
     const response = await fetch(url);
-    // console.log('API Data:', response.data);
     const data = await response.json();
     return data;
   } catch (error) {
@@ -72,45 +70,3 @@ const rocketsSlice = createSlice({
 export const { reservation } = rocketsSlice.actions;
 
 export default rocketsSlice.reducer;
-
-// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
-
-// const apiUrl = 'https://api.spacexdata.com/v4/rockets';
-
-// // Define an async thunk action to fetch rockets
-// export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
-//   try {
-//     const response = await axios.get(apiUrl);
-//     return response.data;
-//   } catch (error) {
-//     throw new Error(error.message);
-//   }
-// });
-
-// const rocketsSlice = createSlice({
-//   name: 'rockets',
-//   initialState: {
-//     data: [],
-//     isLoading: false,
-//     error: null,
-//   },
-//   reducers: {},
-//   extraReducers(builder) {
-//     builder
-//       .addCase(fetchRockets.pending, (state) => {
-//         state.isLoading = true;
-//         state.error = null;
-//       })
-//       .addCase(fetchRockets.fulfilled, (state, action) => {
-//         state.isLoading = false;
-//         state.data = action.payload;
-//       })
-//       .addCase(fetchRockets.rejected, (state, action) => {
-//         state.isLoading = false;
-//         state.error = action.error.message;
-//       });
-//   },
-// });
-
-// export default rocketsSlice.reducer;
